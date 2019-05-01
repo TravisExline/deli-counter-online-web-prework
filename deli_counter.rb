f8e7a8b740df8e9793841d996bc82c0a1c3c38b4
Travis Exline <t.exline22@gmail.com>
2019-05-01 15:13:43 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,3 +14,10 @@ def take_a_number(deli_line, name)
   deli_line << name
   puts "Welcome, #{name}. You are number #{deli_line.length} in line."
 end
+
+def now_serving(deli_name)
+  if deli_name === 0
+    puts "There is nobody waiting to be served!"
+  end
+end
+    
