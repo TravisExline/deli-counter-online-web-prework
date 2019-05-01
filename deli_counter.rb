faad786848faa38ccfd3d9d63c0856de39bab993
Travis Exline <t.exline22@gmail.com>
2019-05-01 14:32:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,10 +2,9 @@ def line(line_length)
  if line_length.empty?
    puts "The line is currently empty."
  else
-   phrase = "The line is currently:"
    line_length.each.with_index(1) do |person, num|
-     phrase << " #{num}. #{person}"
-   puts phrase
+      puts "The line is currently: #{num}. #{person}"
+
   end
  end
 end
